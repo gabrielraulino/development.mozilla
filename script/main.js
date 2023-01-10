d0574e145da921dc8d1a27f01928ffc850909d11
gabrielraulino <gabrielviana@alu.ufc.br>
2023-01-10 11:20:13 -0300

    feat: primeira parte funcional

--- script/main.js
+++ script/main.js
@@ -1,2 +1,35 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Olá mundo"
+const myImage = document.querySelector("img");
+
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/rinoceronte-negro.webp") {
+    myImage.setAttribute("src", "images/mascotejs.jpg");
+  } else {
+    myImage.setAttribute("src", "images/rinoceronte-negro.webp");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+let myName = null;
+function setUserName() {
+  myName = prompt('Por favor, insira seu nome.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Javascript é legal, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Javascript é legal, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
